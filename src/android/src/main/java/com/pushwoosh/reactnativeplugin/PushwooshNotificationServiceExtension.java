e2fcf2b4edd6d8be1b1b7f621b982c5e517a0102
marint <teppo.marin@gmail.com>
2020-12-07 12:52:28 +0200

    Send broadcast message for silent pushes

--- src/android/src/main/java/com/pushwoosh/reactnativeplugin/PushwooshNotificationServiceExtension.java
+++ src/android/src/main/java/com/pushwoosh/reactnativeplugin/PushwooshNotificationServiceExtension.java
@@ -2,6 +2,7 @@ package com.pushwoosh.reactnativeplugin;
 
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.Intent;
 
 import com.pushwoosh.internal.utils.PWLog;
 import com.pushwoosh.notification.NotificationServiceExtension;
@@ -11,6 +12,13 @@ public class PushwooshNotificationServiceExtension extends NotificationServiceEx
 
 	private boolean showForegroundPush;
 
+	private boolean silentPushBroadcastEnabled;
+	private String silentPushBroadcastAction;
+	private String silentPushBroadcastParameter;
+
+	private static final String SILENT_PUSH_BROADCAST_ACTION_DEFAULT = "MESSAGE";
+	private static final String SILENT_PUSH_BROADCAST_PARAMETER_DEFAULT = "message";
+
 	public PushwooshNotificationServiceExtension() {
 		try {
 			String packageName = getApplicationContext().getPackageName();
@@ -18,6 +26,11 @@ public class PushwooshNotificationServiceExtension extends NotificationServiceEx
 
 			if (ai.metaData != null) {
 				showForegroundPush = ai.metaData.getBoolean("PW_BROADCAST_PUSH", false) || ai.metaData.getBoolean("com.pushwoosh.foreground_push", false);
+				silentPushBroadcastEnabled = ai.metaData.getBoolean("com.pushwoosh.silent_push_broadcast_enabled", false);
+				if (silentPushBroadcastEnabled) {
+					silentPushBroadcastAction = ai.metaData.getString("com.pushwoosh.silent_push_broadcast_action", SILENT_PUSH_BROADCAST_ACTION_DEFAULT);
+					silentPushBroadcastParameter = ai.metaData.getString("com.pushwoosh.silent_push_broadcast_parameter", SILENT_PUSH_BROADCAST_PARAMETER_DEFAULT);
+				}
 			}
 		} catch (Exception e) {
 			PWLog.exception(e);
@@ -28,8 +41,28 @@ public class PushwooshNotificationServiceExtension extends NotificationServiceEx
 
 	@Override
 	protected boolean onMessageReceived(final PushMessage pushMessage) {
+		if (silentPushBroadcastEnabled && pushMessage != null && pushMessage.isSilent()) {
+			sendBroadcastMessage(pushMessage.getCustomData());
+		}
 		PushwooshPlugin.messageReceived(pushMessage.toJson().toString());
 		return (!showForegroundPush && isAppOnForeground()) || super.onMessageReceived(pushMessage);
+	}
+
+
+	protected void sendBroadcastMessage(String data) {
+		if (data != null && data.length() > 0) {
+			PWLog.debug(PushwooshPlugin.TAG, "Broadcast silent push message: " + data);
+
+			// send broadcast event
+			Intent broadcastIntent = new Intent();
+			String packageName = getApplicationContext().getPackageName();
+			broadcastIntent.setAction(packageName + "." + silentPushBroadcastAction);
+			// send broadcast intent only for current application (explicit broadcast)
+			broadcastIntent.setPackage(packageName);
+			broadcastIntent.putExtra(silentPushBroadcastParameter, data);
+
+			getApplicationContext().sendBroadcast(broadcastIntent);
+		}
 	}
 
 	@Override
